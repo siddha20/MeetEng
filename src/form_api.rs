90e8cdd6f3e59a1d1e54aa3a705061f0d02355f1
siddha <siddhakilaru@gmail.com>
2022-08-02 14:35:38 -0400

    more progress

--- src/form_api.rs
+++ src/form_api.rs
@@ -24,6 +24,10 @@ mod test {
     use super::*; 
 
     #[test]
+    /*
+     * This test asserts that a student was able to be extracted from JSON data
+     * given that the JSON data was valid.
+     */ 
     fn test_json_parsing_student(){
         let valid_student = getStudentJson();
         match make_student_from_form(valid_student) {
@@ -33,6 +37,10 @@ mod test {
     }
 
     #[test]
+    /*
+     * This test asserts that a student was not able to be extracted from JSON
+     *  data given that the JSON data was invalid.
+     */ 
     fn test_json_parsing_student_error(){
         let invalid_student = getStudentJson();
         match make_student_from_form(valid_student) {
@@ -42,6 +50,10 @@ mod test {
     }
 
     #[test]
+    /*
+     * This test asserts that a mentor was able to be extracted from JSON data
+     * given that the JSON data was valid.
+     */ 
     fn test_json_parsing_mentor(){
         let valid_mentor = getMentorJson();
         match make_mentor_from_form(valid_mentor) {
@@ -51,6 +63,10 @@ mod test {
     }
     
     #[test]
+    /*
+     * This test asserts that a mentor was not able to be extracted from JSON
+     *  data given that the JSON data was invalid.
+     */ 
     fn test_json_parsing_mentor_error(){
         let invalid_mentor = getMentorJson();
         match make_mentor_from_form(valid_mentor) {
