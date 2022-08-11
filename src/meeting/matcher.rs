f6611abec577f5f92a3046f8dc007895c7c90a5f
siddhak <siddhakilaru>
2022-08-11 11:48:07 -0400

    progress

--- src/meeting/matcher.rs
+++ src/meeting/matcher.rs
@@ -6,13 +6,84 @@ pub struct People {
 }
 
 // weights for similarity metric
-struct weights {
-    availability: f64,
-    major: f64,
-    interests: f64,
-    gender: f64,
-    sports: f64, 
-    rotc: f64
+pub struct Weights {
+    pub avail: f64,
+    pub major: f64,
+    pub interests: f64,
+    pub gender: f64,
+    pub sports: f64, 
+    pub rotc: f64
 }
 
-// for time we should use the l2 norm and f(x) = 1 - x/(x+100)
+pub struct Metrics {
+    pub weights: Weights,
+}
+
+impl Weights {
+    pub fn new() -> Self {
+        Weights {
+            avail: 1.0,
+            major: 1.0,
+            interests: 1.0,
+            gender: 1.0,
+            sports: 1.0,
+            rotc: 1.0
+        }
+    }
+}
+
+
+impl Metrics {
+    pub fn new() -> Self {
+        Metrics {
+            weights: Weights::new(),
+        }
+    }
+
+    pub fn value(&self, a: &Student, b: &Mentor) -> f64 {
+        (self.weights.avail * self.avail_sim(&a, &b)) +
+        (self.weights.major * self.major_sim(&a, &b)) +
+        (self.weights.interests * self.interests_sim(&a, &b)) +
+        (self.weights.gender * self.gender_sim(&a, &b)) +
+        (self.weights.sports * self.sports_sim(&a, &b)) +
+        (self.weights.rotc * self.rotc_sim(&a, &b))
+    }
+
+    // for time we should use the l2 norm and f(x) = 1 - x/(x+100)
+    fn avail_sim(&self, a: &Student, b: &Mentor) -> f64 {
+        let res = Time::l2(&a.availability, &b.availability);
+        (1.0 - res/(res + 100.0))
+    }
+
+    fn major_sim(&self, a: &Student, b: &Mentor) -> f64 {
+        0.0
+    }
+
+    fn interests_sim(&self, a: &Student, b: &Mentor) -> f64 {
+        0.0
+    }
+
+    fn gender_sim(&self, a: &Student, b: &Mentor) -> f64 {
+        0.0
+    }
+
+    fn sports_sim(&self, a: &Student, b: &Mentor) -> f64 {
+        0.0
+    }
+
+    fn rotc_sim(&self, a: &Student, b: &Mentor) -> f64 {
+        0.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_metric_value() {
+        assert!(true);
+    }
+}
+
+
--- src/meeting/time.rs
+++ src/meeting/time.rs
@@ -24,24 +24,30 @@ impl Time {
          (self.day as f64).powf(2.0) +
          (self.time as f64).powf(2.0)).sqrt()
     }
+
+    // cosine similarity
+    pub fn cos_sim(a: &Time, b: &Time) -> f64 {
+        ((a.year as f64 * b.year as f64) +
+        (a.month as f64 * b.month as f64) + 
+        (a.day as f64 * b.day as f64) +
+        (a.time as f64 * b.time as f64))/
+        (a.mag() * b.mag())
+    }
+
+    // L2 norm
+    pub fn l2(a: &Time, b: &Time) -> f64 {
+        (((a.year - b.year) as f64 * 525600.0).powf(2.0) +
+        ((a.month - b.month) as f64 * 43800.0).powf(2.0) +
+        ((a.day - b.day) as f64 * 1440.0).powf(2.0) +
+        ((a.time - b.time) as f64).powf(2.0)).sqrt()
+    }
 }
 
-// cosine similarity
-pub fn cos_sim(a: &Time, b: &Time) -> f64 {
-    ((a.year as f64 * b.year as f64) +
-     (a.month as f64 * b.month as f64) + 
-     (a.day as f64 * b.day as f64) +
-     (a.time as f64 * b.time as f64))/
-     (a.mag() * b.mag())
-}
 
-// L2 norm
-pub fn l2(a: &Time, b: &Time) -> f64 {
-    (((a.year - b.year) as f64 * 525600.0).powf(2.0) +
-    ((a.month - b.month) as f64 * 43800.0).powf(2.0) +
-    ((a.day - b.day) as f64 * 1440.0).powf(2.0) +
-    ((a.time - b.time) as f64).powf(2.0)).sqrt()
-}
+
+
+
+
 
 #[cfg(test)]
 mod tests {
@@ -56,13 +62,13 @@ mod tests {
             time: 1000
         };
         let b = Time{
-            year: 2022,
+            year: 2021,
             month: 08,
             day: 15,
             time: 1030
         };
-        let res = l2(&a, &b);
-        println!("result: {} scaled: {}", res, 1.0 - (res/(res + 100.0)));
+        let res = Time::l2(&a, &b);
+        println!("result: {}", res);
         assert!(true);
     }
 }
